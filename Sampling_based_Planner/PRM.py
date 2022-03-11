4fd9c5f0c5b2106022a39e43033ba64f1c6f37c3
Prasham Patel <91484142+Prasham-Patel@users.noreply.github.com>
2022-03-11 14:13:58 -0500

    Add files via upload

---
+++ Sampling_based_Planner/PRM.py
@@ -0,0 +1,249 @@
+# Standard Algorithm Implementation
+# Sampling-based Algorithms PRM
+
+import matplotlib.pyplot as plt
+import numpy as np
+import networkx as nx
+
+
+# Class for PRM
+class PRM:
+    # Constructor
+    def __init__(self, map_array):
+        self.map_array = map_array            # map array, 1->free, 0->obstacle
+        self.size_row = map_array.shape[0]    # map size
+        self.size_col = map_array.shape[1]    # map size
+
+        self.samples = []                     # list of sampled points
+        self.graph = nx.Graph()               # constructed graph
+        self.path = []                        # list of nodes of the found path
+
+
+    def check_collision(self, p1, p2):
+        '''Check if the path between two points collide with obstacles
+        arguments:
+            p1 - point 1, [row, col]
+            p2 - point 2, [row, col]
+
+        return:
+            True if there are obstacles between two points
+        '''
+        ### YOUR CODE HERE ###
+        return True
+
+
+    def dis(self, point1, point2):
+        '''Calculate the euclidean distance between two points
+        arguments:
+            p1 - point 1, [row, col]
+            p2 - point 2, [row, col]
+
+        return:
+            euclidean distance between two points
+        '''
+        ### YOUR CODE HERE ###
+        return 0
+
+
+    def uniform_sample(self, n_pts):
+        '''Use uniform sampling and store valid points
+        arguments:
+            n_pts - number of points try to sample, 
+                    not the number of final sampled points
+
+        check collision and append valide points to self.samples
+        as [(row1, col1), (row2, col2), (row3, col3) ...]
+        '''
+        # Initialize graph
+        self.graph.clear()
+
+        ### YOUR CODE HERE ###
+        self.samples.append((0, 0))
+
+    
+    def random_sample(self, n_pts):
+        '''Use random sampling and store valid points
+        arguments:
+            n_pts - number of points try to sample, 
+                    not the number of final sampled points
+
+        check collision and append valide points to self.samples
+        as [(row1, col1), (row2, col2), (row3, col3) ...]
+        '''
+        # Initialize graph
+        self.graph.clear()
+
+        ### YOUR CODE HERE ###
+        self.samples.append((0, 0))
+
+
+    def gaussian_sample(self, n_pts):
+        '''Use gaussian sampling and store valid points
+        arguments:
+            n_pts - number of points try to sample, 
+                    not the number of final sampled points
+
+        check collision and append valide points to self.samples
+        as [(row1, col1), (row2, col2), (row3, col3) ...]
+        '''
+        # Initialize graph
+        self.graph.clear()
+
+        ### YOUR CODE HERE ###
+        self.samples.append((0, 0))
+
+
+    def bridge_sample(self, n_pts):
+        '''Use bridge sampling and store valid points
+        arguments:
+            n_pts - number of points try to sample, 
+                    not the number of final sampled points
+
+        check collision and append valide points to self.samples
+        as [(row1, col1), (row2, col2), (row3, col3) ...]
+        '''
+        # Initialize graph
+        self.graph.clear()
+
+        ### YOUR CODE HERE ###
+        self.samples.append((0, 0))
+
+
+    def draw_map(self):
+        '''Visualization of the result
+        '''
+        # Create empty map
+        fig, ax = plt.subplots()
+        img = 255 * np.dstack((self.map_array, self.map_array, self.map_array))
+        ax.imshow(img)
+
+        # Draw graph
+        # get node position (swap coordinates)
+        node_pos = np.array(self.samples)[:, [1, 0]]
+        pos = dict( zip( range( len(self.samples) ), node_pos) )
+        pos['start'] = (self.samples[-2][1], self.samples[-2][0])
+        pos['goal'] = (self.samples[-1][1], self.samples[-1][0])
+        
+        # draw constructed graph
+        nx.draw(self.graph, pos, node_size=3, node_color='y', edge_color='y' ,ax=ax)
+
+        # If found a path
+        if self.path:
+            # add temporary start and goal edge to the path
+            final_path_edge = list(zip(self.path[:-1], self.path[1:]))
+            nx.draw_networkx_nodes(self.graph, pos=pos, nodelist=self.path, node_size=8, node_color='b')
+            nx.draw_networkx_edges(self.graph, pos=pos, edgelist=final_path_edge, width=2, edge_color='b')
+
+        # draw start and goal
+        nx.draw_networkx_nodes(self.graph, pos=pos, nodelist=['start'], node_size=12,  node_color='g')
+        nx.draw_networkx_nodes(self.graph, pos=pos, nodelist=['goal'], node_size=12,  node_color='r')
+
+        # show image
+        plt.axis('on')
+        ax.tick_params(left=True, bottom=True, labelleft=True, labelbottom=True)
+        plt.show()
+
+
+    def sample(self, n_pts=1000, sampling_method="uniform"):
+        '''Construct a graph for PRM
+        arguments:
+            n_pts - number of points try to sample, 
+                    not the number of final sampled points
+            sampling_method - name of the chosen sampling method
+
+        Sample points, connect, and add nodes and edges to self.graph
+        '''
+        # Initialize before sampling
+        self.samples = []
+        self.graph.clear()
+        self.path = []
+
+        # Sample methods
+        if sampling_method == "uniform":
+            self.uniform_sample(n_pts)
+        elif sampling_method == "random":
+            self.random_sample(n_pts)
+        elif sampling_method == "gaussian":
+            self.gaussian_sample(n_pts)
+        elif sampling_method == "bridge":
+            self.bridge_sample(n_pts)
+
+        ### YOUR CODE HERE ###
+
+        # Find the pairs of points that need to be connected
+        # and compute their distance/weight.
+        # Store them as
+        # pairs = [(p_id0, p_id1, weight_01), (p_id0, p_id2, weight_02), 
+        #          (p_id1, p_id2, weight_12) ...]
+        pairs = []
+
+        # Use sampled points and pairs of points to build a graph.
+        # To add nodes to the graph, use
+        # self.graph.add_nodes_from([p_id0, p_id1, p_id2 ...])
+        # To add weighted edges to the graph, use
+        # self.graph.add_weighted_edges_from([(p_id0, p_id1, weight_01), 
+        #                                     (p_id0, p_id2, weight_02), 
+        #                                     (p_id1, p_id2, weight_12) ...])
+        # 'p_id' here is an integer, representing the order of 
+        # current point in self.samples
+        # For example, for self.samples = [(1, 2), (3, 4), (5, 6)],
+        # p_id for (1, 2) is 0 and p_id for (3, 4) is 1.
+        self.graph.add_nodes_from([])
+        self.graph.add_weighted_edges_from(pairs)
+
+        # Print constructed graph information
+        n_nodes = self.graph.number_of_nodes()
+        n_edges = self.graph.number_of_edges()
+        print("The constructed graph has %d nodes and %d edges" %(n_nodes, n_edges))
+
+
+    def search(self, start, goal):
+        '''Search for a path in graph given start and goal location
+        arguments:
+            start - start point coordinate [row, col]
+            goal - goal point coordinate [row, col]
+
+        Temporary add start and goal node, edges of them and their nearest neighbors
+        to graph for self.graph to search for a path.
+        '''
+        # Clear previous path
+        self.path = []
+
+        # Temporarily add start and goal to the graph
+        self.samples.append(start)
+        self.samples.append(goal)
+        # start and goal id will be 'start' and 'goal' instead of some integer
+        self.graph.add_nodes_from(['start', 'goal'])
+
+        ### YOUR CODE HERE ###
+
+        # Find the pairs of points that need to be connected
+        # and compute their distance/weight.
+        # You could store them as
+        # start_pairs = [(start_id, p_id0, weight_s0), (start_id, p_id1, weight_s1), 
+        #                (start_id, p_id2, weight_s2) ...]
+        start_pairs = []
+        goal_pairs = []
+
+        # Add the edge to graph
+        self.graph.add_weighted_edges_from(start_pairs)
+        self.graph.add_weighted_edges_from(goal_pairs)
+        
+        # Seach using Dijkstra
+        try:
+            self.path = nx.algorithms.shortest_paths.weighted.dijkstra_path(self.graph, 'start', 'goal')
+            path_length = nx.algorithms.shortest_paths.weighted.dijkstra_path_length(self.graph, 'start', 'goal')
+            print("The path length is %.2f" %path_length)
+        except nx.exception.NetworkXNoPath:
+            print("No path found")
+        
+        # Draw result
+        self.draw_map()
+
+        # Remove start and goal node and their edges
+        self.samples.pop(-1)
+        self.samples.pop(-1)
+        self.graph.remove_nodes_from(['start', 'goal'])
+        self.graph.remove_edges_from(start_pairs)
+        self.graph.remove_edges_from(goal_pairs)
+        
---
+++ Sampling_based_Planner/RRT.py
@@ -0,0 +1,206 @@
+# Standard Algorithm Implementation
+# Sampling-based Algorithms RRT and RRT*
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# Class for each tree node
+class Node:
+    def __init__(self, row, col):
+        self.row = row        # coordinate
+        self.col = col        # coordinate
+        self.parent = None    # parent node
+        self.cost = 0.0       # cost
+
+
+# Class for RRT
+class RRT:
+    # Constructor
+    def __init__(self, map_array, start, goal):
+        self.map_array = map_array            # map array, 1->free, 0->obstacle
+        self.size_row = map_array.shape[0]    # map size
+        self.size_col = map_array.shape[1]    # map size
+
+        self.start = Node(start[0], start[1]) # start node
+        self.goal = Node(goal[0], goal[1])    # goal node
+        self.vertices = []                    # list of nodes
+        self.found = False                    # found flag
+        
+
+    def init_map(self):
+        '''Intialize the map before each search
+        '''
+        self.found = False
+        self.vertices = []
+        self.vertices.append(self.start)
+
+    
+    def dis(self, node1, node2):
+        '''Calculate the euclidean distance between two nodes
+        arguments:
+            node1 - node 1
+            node2 - node 2
+
+        return:
+            euclidean distance between two nodes
+        '''
+        ### YOUR CODE HERE ###
+        return 0
+
+    
+    def check_collision(self, node1, node2):
+        '''Check if the path between two nodes collide with obstacles
+        arguments:
+            node1 - node 1
+            node2 - node 2
+
+        return:
+            True if the new node is valid to be connected
+        '''
+        ### YOUR CODE HERE ###
+        return True
+
+
+    def get_new_point(self, goal_bias):
+        '''Choose the goal or generate a random point
+        arguments:
+            goal_bias - the possibility of choosing the goal instead of a random point
+
+        return:
+            point - the new point
+        '''
+        ### YOUR CODE HERE ###
+        return self.goal
+
+    
+    def get_nearest_node(self, point):
+        '''Find the nearest node in self.vertices with respect to the new point
+        arguments:
+            point - the new point
+
+        return:
+            the nearest node
+        '''
+        ### YOUR CODE HERE ###
+        return self.vertices[0]
+
+
+    def get_neighbors(self, new_node, neighbor_size):
+        '''Get the neighbors that are within the neighbor distance from the node
+        arguments:
+            new_node - a new node
+            neighbor_size - the neighbor distance
+
+        return:
+            neighbors - a list of neighbors that are within the neighbor distance 
+        '''
+        ### YOUR CODE HERE ###
+        return [self.vertices[0]]
+
+
+    def rewire(self, new_node, neighbors):
+        '''Rewire the new node and all its neighbors
+        arguments:
+            new_node - the new node
+            neighbors - a list of neighbors that are within the neighbor distance from the node
+
+        Rewire the new node if connecting to a new neighbor node will give least cost.
+        Rewire all the other neighbor nodes.
+        '''
+        ### YOUR CODE HERE ###
+
+    
+    def draw_map(self):
+        '''Visualization of the result
+        '''
+        # Create empty map
+        fig, ax = plt.subplots(1)
+        img = 255 * np.dstack((self.map_array, self.map_array, self.map_array))
+        ax.imshow(img)
+
+        # Draw Trees or Sample points
+        for node in self.vertices[1:-1]:
+            plt.plot(node.col, node.row, markersize=3, marker='o', color='y')
+            plt.plot([node.col, node.parent.col], [node.row, node.parent.row], color='y')
+        
+        # Draw Final Path if found
+        if self.found:
+            cur = self.goal
+            while cur.col != self.start.col and cur.row != self.start.row:
+                plt.plot([cur.col, cur.parent.col], [cur.row, cur.parent.row], color='b')
+                cur = cur.parent
+                plt.plot(cur.col, cur.row, markersize=3, marker='o', color='b')
+
+        # Draw start and goal
+        plt.plot(self.start.col, self.start.row, markersize=5, marker='o', color='g')
+        plt.plot(self.goal.col, self.goal.row, markersize=5, marker='o', color='r')
+
+        # show image
+        plt.show()
+
+
+    def RRT(self, n_pts=1000):
+        '''RRT main search function
+        arguments:
+            n_pts - number of points try to sample, 
+                    not the number of final sampled points
+
+        In each step, extend a new node if possible, and check if reached the goal
+        '''
+        # Remove previous result
+        self.init_map()
+
+        ### YOUR CODE HERE ###
+
+        # In each step,
+        # get a new point, 
+        # get its nearest node, 
+        # extend the node and check collision to decide whether to add or drop,
+        # if added, check if reach the neighbor region of the goal.
+
+        # Output
+        if self.found:
+            steps = len(self.vertices) - 2
+            length = self.goal.cost
+            print("It took %d nodes to find the current path" %steps)
+            print("The path length is %.2f" %length)
+        else:
+            print("No path found")
+        
+        # Draw result
+        self.draw_map()
+
+
+    def RRT_star(self, n_pts=1000, neighbor_size=20):
+        '''RRT* search function
+        arguments:
+            n_pts - number of points try to sample, 
+                    not the number of final sampled points
+            neighbor_size - the neighbor distance
+        
+        In each step, extend a new node if possible, and rewire the node and its neighbors
+        '''
+        # Remove previous result
+        self.init_map()
+
+        ### YOUR CODE HERE ###
+
+        # In each step,
+        # get a new point, 
+        # get its nearest node, 
+        # extend the node and check collision to decide whether to add or drop,
+        # if added, rewire the node and its neighbors,
+        # and check if reach the neighbor region of the goal if the path is not found.
+
+        # Output
+        if self.found:
+            steps = len(self.vertices) - 2
+            length = self.goal.cost
+            print("It took %d nodes to find the current path" %steps)
+            print("The path length is %.2f" %length)
+        else:
+            print("No path found")
+
+        # Draw result
+        self.draw_map()
